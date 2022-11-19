84d76285825ab9862a33c32565b2545363876963
adam <adame.barhdadi@student.vinci.be>
2022-11-19 19:34:54 +0100

    frontedn

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,17 +7,48 @@
  * - the router will show the Page associated to this URI when the user click on a nav-link
  */
 
-const Navbar = () => {
+ import { getAuthenticatedUser, getAnonymUser } from "../../utils/auths";
+
+ const Navbar = () => {
+const userAnonyme=getAnonymUser.usernam
+  console.log(getAuthenticatedUser,userAnonyme)
   const navbarWrapper = document.querySelector('#navbarWrapper');
-  const navbar = `
-  <h1>
-  DISCOVER OR RE  <span style="color:#FFFF8F">DISCOVER THE </span> <span style="color:#C41E3A">CAPITAL OF EUROPE	</span>
- </h1>
+   let navbar='';
+
+ // if(!getAnonymUser && !getAuthenticatedUser){
+  
+ navbar += `
+   <h1>
+   DISCOVER OR RE  <span style="color:#FFFF8F">DISCOVER THE </span> <span style="color:#C41E3A">CAPITAL OF EUROPE	</span>
+  </h1>
+   `;
+/* }else{
+  navbar += `
+  <ul>
+  <li >
+    <a data-uri="/Ranking">Ranking</a>
+
+  ${getAuthenticatedUser ? ' <li> <a data-uri="/Ranking">MyStats</a> </li>' : ''}
+  ${getAnonymUser ? ' <li> <a data-uri="/Ranking">Login/Register</a> </li>' : ''}
+    <li>
+    <a data-uri="/Ranking">Contact</a>
+   </li> 
+   </li> 
+  </ul>
+  `;
+ }
+ */
+ 
+ 
+ navbar += `
  <hr class="hr1">  
  <hr class="hr2">  
-<hr class="hr3"> 
-  `;
-  navbarWrapper.innerHTML = navbar;
-};
-
-export default Navbar;
+ <hr class="hr3"> 
+ `;
+  
+   
+   navbarWrapper.innerHTML = navbar;
+ };
+ 
+ export default Navbar;
+ 
--- src/Components/Pages/HomePage.js
+++ src/Components/Pages/HomePage.js
@@ -1,5 +1,7 @@
 import UserIcon1 from '../../img/userI1.jpg';
 import UserIcon2 from '../../img/userI2.png';
+import { setAnonymeUser, setAuthenticatedUser } from '../../utils/auths';
+import Navbar from '../Navbar/Navbar';
 
 const homePage=`
   
@@ -8,17 +10,17 @@ const homePage=`
       <div class="container">  
         
     <div>
-          <input type="text" placeholder="Enter Username" name="username" required>  
+          <input type="text" placeholder="Enter Username" id="username" required/>  
  </div>  
            <div>
        
-          <input type="password" placeholder="Enter Password" name="password" required>  
+          <input type="password" placeholder="Enter Password" id="password"  required/>
       </div>  
-           <div>
-            <button type="submit">Login</button>  
+           <div >
+            <input type="submit"  id="login" value="Login"/>
              </div>  
              <div>
-             <button type="submit">Register</button> 
+             <input type="submit" id="register"  value="Register"/>
        </div>  
          
              <div>
@@ -33,10 +35,10 @@ const homePage=`
       <div class="container">  
         
     <div>
-          <input type="text" placeholder="Enter Username" name="username" required>  
+          <input type="text" placeholder="Enter Username" name="username" id="usernameAnonym" required/>  
  </div>  
            <div>
-       <button type="submit">Play Without An Account (No classement Provided)</button>  
+           <input type="submit" id="anonymLogin"  value="Play Without An Account (No classement Provided)"/>
              </div>  
              <div>
 
@@ -48,8 +50,88 @@ const homePage=`
 `
 
 const HomePage = () => {
-  const main = document.querySelector('main');
+  const main = document.querySelector('main');  
   main.innerHTML = homePage
+
+  document.addEventListener('submit',async (e)=>{
+    e.preventDefault();
+    const type=e.submitter.id
+    const username=document.querySelector('#username').value;
+    const password=document.querySelector('#password').value;
+    const usernameAnonyme=document.querySelector('#usernameAnonym').value;
+  
+   if(type==="login"){
+    
+    const OPTIONS={
+      method:'POST',
+      body:JSON.stringify({
+        username,
+        password
+      }),
+      headers:{
+        'Content-Type':'application/json' ,
+      }
+
+    };
+   const response=await fetch('/api/auth/login',OPTIONS);
+   if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+   const authenticatedUser = await response.json();
+   setAuthenticatedUser(authenticatedUser)
+  }
+  
+  if(type==="register"){
+
+   const OPTIONS={
+    method:'POST',
+    body:JSON.stringify({
+      username,
+      password
+    }),
+    headers:{
+      'Content-Type':'application/json' ,
+    }
+
+  };
+ const response=await fetch('/api/auth/register',OPTIONS);
+ if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+ const authenticatedUser = await response.json();
+setAuthenticatedUser(authenticatedUser)
+
+  }
+
+  if(type==="anonymLogin"){
+ const OPTIONS={
+  method:'POST',
+  body:JSON.stringify({
+    username:usernameAnonyme,
+  }),
+  headers:{
+    'Content-Type':'application/json' ,
+  }
+
 };
+const response=await fetch('/api/auth/loginAnonyme',OPTIONS);
+
+if (!response.ok) throw new Error(`fetch error : ${response.status} : ${response.statusText}`);
+
+const authenticatedUser = await response.json();
+   setAnonymeUser(authenticatedUser); 
+
+
+
+  }
+
+  Navbar();
+});
+
+
+
+};
+
+
+
+
 
 export default HomePage;
